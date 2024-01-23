e95b822e4fccc2f46dcda50cf4d6ba7031e64267
imyroslav <imyroslav@ukr.net>
2024-01-23 19:07:20 +0700

    task-3-completed

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,19 @@
+function filterArray(numbers, value) {
+    const arrayFiltered = [];
+    for (const item of numbers) {
+        if(item > value) {
+            arrayFiltered.push(item);
+        }
+    }
+    return arrayFiltered;
+}
 
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); 
+console.log(filterArray([1, 2, 3, 4, 5], 4)); 
+console.log(filterArray([1, 2, 3, 4, 5], 5)); 
+console.log(filterArray([12, 24, 8, 41, 76], 38)); 
+console.log(filterArray([12, 24, 8, 41, 76], 20));
 
 
 
